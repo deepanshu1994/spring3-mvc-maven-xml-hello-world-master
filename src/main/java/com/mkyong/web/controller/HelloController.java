78939a06e270b99abd8f997c695d4c106a14a7ea
deepanshu1994 <47136571+deepanshu1994@users.noreply.github.com>
2019-08-10 00:27:56 +0530

    Update HelloController.java

--- src/main/java/com/mkyong/web/controller/HelloController.java
+++ src/main/java/com/mkyong/web/controller/HelloController.java
@@ -14,6 +14,12 @@ public class HelloController {
 	public String printWelcome(ModelMap model) {
 
 		model.addAttribute("message", "Spring 3 MVC Hello World");
+		model.addAttribute("message", "Spring 3 MVC Hello World");
+		model.addAttribute("message", "Spring 3 MVC Hello World");
+		model.addAttribute("message", "Spring 3 MVC Hello World");
+		model.addAttribute("message", "Spring 3 MVC Hello World");
+		model.addAttribute("message", "Spring 3 MVC Hello World");
+		model.addAttribute("message", "Spring 3 MVC Hello World");
 		return "hello";
 
 	}
